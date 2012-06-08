3d97fffa29aa043ae0ba634533d4b5fdea1a6e0a
root <root@NiXToP.(none)>
2012-06-08 19:37:19 -0400

    Added throughput script

---
+++ gather_throughput.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+# Information gathering script written by Craig Dods
+# 15/02/2011
+# This script is provided for debugging purposes only
+# with no warranty. User assumes all risk
+#
+# Interrupt this script with a Control-C to stop
+#
+
+# Collect stats once per $sleep_time, in sec. Tunable.
+# 5 min by default, 86400 seconds = 1 day
+
+sleep_time=300
+
+# Configure your interfaces here (will be updated to do this automatically in the future)
+int1=Internal
+int2=External
+int3=DMZ
+int4=Lan1
+int5=Lan2
+int6=Lan3
+
+cur_time=`date +'%m%d%H%M%S'`
+LOG_FILE="CP-$cur_time.log"
+
+trap cleanup 2 3 5 6 9 11
+cleanup ()
+{
+	echo > /dev/tty
+	echo "Submit $LOG_FILE to support site" > /dev/tty
+	exit 0
+}
+
+exec_cmd ()
+{
+	echo "================ output of $1 =================="
+	eval $1
+}
+
+# collect stats per $sleep_time.
+while : 
+do
+	cur_time=`date`
+	echo "$cur_time {{{"
+
+		echo "============== Hostname ==============="
+		hostname
+		echo "============== Uptime ==============="
+		uptime
+		echo "============== Network Statistics ==============="
+		echo -n $int1 && ethtool -S $int1 | grep -e rx_bytes -e tx_bytes | sed ':a;$b;N;s/\n//;ba' | sed 's/[rx_bytes:]//g'
+
+
+	echo "}}}"
+	sleep $sleep_time
+done >> $LOG_FILE
+
+
+
+
