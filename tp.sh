29cbb7a3d4349990e1e34ee7491bcfe9e27288fb
Craig Dods <craig.dods@gmail.com>
2012-06-14 13:06:03 -0400

    COMPLETELY NEW TP SCRIPT w/ BURST CALC

--- tp.sh
+++ tp.sh
@@ -6,31 +6,41 @@ read logfile
 
 echo " "
 echo $logfile "has been chosen"
+echo " "
+echo "All values are in mbps (megabits per second) "
+echo "============================================"
+echo "INT AVR_RX AVR_TX  MAX_RX  MAX_TX "
 
 cat $logfile | awk '
-    BEGIN {
-        OFMT = "%.4f"
-    }
+BEGIN {
+    OFMT = "%.4f"
+}
 
-    /^[[:blank:]]*$/ { next }
+/^[[:blank:]]*$/ { next }
 
-    ! ($1 in prevrx) {
-        prevrx[$1] = $2
-        prevtx[$1] = $3
-	next
-        
+! ($1 in prevrx) {
+    prevrx[$1] = $2
+    prevtx[$1] = $3
+    next
+}
+{
+    count[$1]++
+    drx = $2 - prevrx[$1]
+    dtx = $3 - prevtx[$1]
+    rx[$1] += drx
+    tx[$1] += dtx
+    prevrx[$1] = $2
+    prevtx[$1] = $3
+    if (drx > maxdrx[$1]) {
+        maxdrx[$1] = drx
     }
-    {
-        count[$1]++
-        drx = $2 - prevrx[$1]
-        dtx = $3 - prevtx[$1]
-        rx[$1] += drx
-        tx[$1] += dtx
-        prevrx[$1] = $2
-        prevtx[$1] = $3
+    if (dtx > maxdtx[$1]) {
+        maxdtx[$1] = dtx
     }
-    END {
-        for (iface in rx) {
-            print iface, (rx[iface] / 131072 ) / count[iface], (tx[iface] / 131072) / count[iface]
-        }
+}
+END {
+    for (iface in rx) {
+        print iface, (rx[iface] / 131072)  / count[iface], (tx[iface] / 131072) / count[iface], (maxdrx[iface] / 131072), (maxdtx[iface] / 131072)
+    }
 }'
+
