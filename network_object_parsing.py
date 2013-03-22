c9f5e519d8963ee865987a34ad72318328c768f8
craigdods <craig.dods@gmail.com>
2013-03-22 08:48:23 -0300

    Everything works except group objects

--- network_object_parsing.py
+++ network_object_parsing.py
@@ -20,6 +20,15 @@ for network_object in NetworkObjectTag:
         maskElement = network_object.getElementsByTagName("netmask")[0]
         netmask = maskElement.firstChild.data
     #address_ranges
-    ipaddr_first = network_object.getElementsByTagName("ipaddr_first")
-    ipaddr_last = network_object.getElementsByTagName("ipaddr_last")
-    print(name,class_name,ipaddr,netmask,ipaddr_first,ipaddr_last,color)
+    ipaddr_firstElement = network_object.getElementsByTagName("ipaddr_first")
+    if ipaddr_firstElement:
+        ipaddr_firstElement = network_object.getElementsByTagName("ipaddr_first")[0]
+        ipaddr_first = ipaddr_firstElement.firstChild.data
+    ipaddr_lastElement = network_object.getElementsByTagName("ipaddr_last")
+    if ipaddr_lastElement:
+        ipaddr_lastElement = network_object.getElementsByTagName("ipaddr_last")[0]
+        ipaddr_last = ipaddr_lastElement.firstChild.data    
+    if ipaddr_firstElement:
+        print(name,class_name,ipaddr,netmask,ipaddr_first,ipaddr_last,color)
+    else:
+            print(name,class_name,ipaddr,netmask,color)
