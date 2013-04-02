222f4b4312078c97fb2d56db7191800dadbabd65
craigdods <craig.dods@gmail.com>
2013-04-02 13:45:54 -0300

    Update csv_parser.sh

--- csv_parser.sh
+++ csv_parser.sh
@@ -58,6 +58,24 @@ grep -v $bad_grep $input_file | awk NF | awk -F, '$2=="Group"{ g=$1; print "crea
 #grep -v $bad_grep $input_file | awk NF | awk -F, '$2=="Group"{ g=$1; print "create network_object_group",g } $7!="-" && $7!=""&& $7!="Log" && $7!="Any"{print "addelement network_objects " g " \x27\x27 network_objects:" $7}' | grep -v 'tcp\|udp\|Tcp\|Udp\|AH\|AOL\|archie\|Backage\|Bionet-Setup\|Blubster\|Citrix_ICA\|Citrix_ICA_Browsing\|CPD\|CrackDown\|DaCryptic\|DerSphere\|DerSphere_II\|dest-unreach\|echo-reply\|echo-request\|Entrust-Admin\|Entrust-KeyMgmt\|ESP\|Freak2k\|FreeTel-outgoing-client\|FreeTel-outgoing-server\|ftp\|ftp-bidir\|ftp-data\|ftp-pasv\|ftp-port\|FW1\|FW1_clntauth_http\|FW1_clntauth_telnet\|FW1_ica_pull\|FW1_key\|FW1_log\|FW1_mgmt\|GateCrasher\|gftp\|ghttp\|gopher\|GoToMyPC\|gre\|H323\|HackaTack_31785\|HackaTack_31787\|HackaTack_31788\|HackaTack_31789\|HackaTack_31790\|HackaTack_31791\|HackaTack_31792\|Hotline_client\|Hotline_tracker\|http\|https\|HTTP_wo_SCV\|ICKiller\|icmp-proto\|ICQ_locator\|IKE\|imap\|iMesh\|InCommand\|info-reply\|info-req\|irc1\|irc2\|ISAKMP\|Jade\|Kaos\|Kuang2\|ldap\|ldap-ssl\|login\|lpdw0rm\|Madster\|mask-reply\|mask-request\|Mneah\|mountd\|MSExchangeADL\|MSExchangeDirRef\|MSExchangeDirRep\|MSExchangeDirSync\|MSExchangeDS\|MSExchangeDSNSPI\|MSExchangeDSRep\|MSExchangeDSXDS\|MSExchangeIS\|MSExchangeMTA\|MSExchangeRA\|MSExchangeStoreAdm\|MSExchangeSysAtt\|MSExchangeSysAttPriv\|MSN_Messenger_5190\|MSN_Messenger_File_Transfer\|MSN_Messenger_Voice\|MSNP\|MSSQL\|MS-SQL-Monitor\|MS-SQL-Server\|Multidropper\|Napster_Client_6600-6699\|Napster_directory_4444\|Napster_directory_5555\|Napster_directory_6666\|Napster_directory_7777\|Napster_directory_8888_primary\|Napster_redirector\|nbdatagram\|nbname\|nbsession\|NBT\|nfsd\|nfsprog\|nlockmgr\|NoBackO\|OAS-NameServer\|OAS-ORB\|Oracle-1524\|Orbix-1570\|Orbix-1571\|param-prblm\|pcANYWHERE\|pcANYWHERE-data\|pcANYWHERE-stat\|pcnfsd\|pcTELECOMMUTE-FileSync\|pop-3\|Port_6667_trojans\|RainWall_Command\|RainWall_Daemon\|RainWall_Status\|RAT\|RDP\|Real-Audio\|Remote_Storm\|RexxRave\|rpc-111\|rtsp\|securidprop\|sg-aol-messenger\|sg-citrix-metaframe\|sg-direct-connect\|sg-dns\|sg-edonkey\|sg-firewall1\|sg-fw1-client-auth\|sg-gnutella\|sg-hotline\|sg-icmp\|sg-ipsec\|sg-irc\|sg-msn-messenger\|sg-napster\|sg-nbt\|sg-netmeeting\|sg-pc-anywhere\|sg-pptp\|sg-time\|sg-wins-services\|sg-yahoo-messenger\|Shadyshell\|SKIP\|SkyDance-T\|smtp\|snmp\|snmp-read\|snmp-trap\|SNTP\|SocketsdesTroie\|sqlnet1\|sqlnet2-1521\|sqlnet2-1525\|sqlnet2-1526\|ssh\|StoneBeat-Control\|StoneBeat-Daemon\|SubSeven\|SubSeven-G\|syslog\|T.120\|TACACSplus\|telnet\|Terrortrojan\|tftp\|TheFlu\|time-exceeded\|timestamp\|timestamp-reply\|TransScout\|Trinoo\|UltorsTrojan\|userdef-SGS-SRL-423\|usserdef-SGS-mng-2456\|VPN1_IPSEC_encapsulation\|winframe\|WinHole\|Xanadu\|Yahoo_Messenger_messages\|Yahoo_Messenger_Webcams\|ypbind\|yppasswd\|ypserv\|ypupdated\|ypxfrd\|ZSP' >> $final
 grep -v $bad_grep $input_file | awk NF | awk -F, '$2=="Group"{ g=$1; print "update network_objects",g }' >> $final
 
+# Complicated rule creation section
+# Determine policy name:
+Rules=tmp_rule_holder.txt
+#Extract the rulebase from the original CSV to ease parsing:
+cat $input_file | sed -n '/Security Policy:/,/Top of table/p' | grep -v "Top of table" >> $Rules
+
+PolName=`grep "Security Policy:" $Rules | awk -F, '{print $1}' | awk '{print $3}'`
+PName_col=$PolName\_scripted
+PName=##$PolName\_scripted
+
+#Create the new rulebase and default rule0
+echo "create policies_collection" $PName_col
+echo "update policies_collections" $PName
+echo "create firewall_policy" $PName
+echo "modify fw_policies" $PName "collection policies_collections:"$PName_col
+echo "addelement fw_policies" $PName "rule security_header_rule"
+echo "addelement fw_policies" $PName "rule:0:action drop_action:drop"
+
 
 line_count=`wc -l $final | awk '{print $1}'`
 echo " "
