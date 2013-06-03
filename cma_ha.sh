3b1b8405d4c866b4225336089e08aa6e1fc1df06
craigdods <craig.dods@gmail.com>
2013-06-03 15:06:57 -0400

    Update cma_ha.sh

--- cma_ha.sh
+++ cma_ha.sh
@@ -1,26 +1,38 @@
-#!/bin/bash
-# CMA HA Sync state checker
-# Written by Craig Dods
+#!/bin/sh
+# Manual configuration file required in /opt/devmgmt/etc/cp.cma.hapairs
+# Configuration should be listed in two columns. Column A should be the primary CMA that resides on the current container. Column B should be the secondary CMA that resides elsewhere.
 
-# Source environment
+# Example:
+# cat /opt/devmgmt/etc/cp.cma.hapairs 
+#CMA_1		CMA_2
+#CMA_3		CMA_4
+#CMA_5		CMA_6
+#CMA_7		CMA_8
+
 . /etc/profile.d/CP.sh
 
-# Example for cma_ha_a - probably want some type of loop here...
-################################################################################################################
-echo "Results for cma_ha_a"
-mdsenv cma_ha_a
-cma_stat=`cpmistat -o schema -r mg cma_ha_b | grep mgStatusOK | sed 's/(//g;s/)//g;s/:mgStatusOK//g;s/^[ \t]*//'`
+if [ -f /opt/devmgmt/etc/cp.cma.hapairs ] ; then
+	while read PRIMARY SECONDARY ; do
 
-  if [ $cma_stat = 0 ]
-	then
-	#Fire Check and record the following in the ticket
-	cpmistat -o schema -r mg cma_a_secondary | grep 'mgStatusOK\|mgSync'
-	fi
-	
-################################################################################################################
+		mdsenv $PRIMARY
+		cma_stat=`cpmistat -o schema -r mg $SECONDARY | grep mgStatusOK | sed 's/(//g;s/)//g;s/:mgStatusOK//g;s/^[ \t]*//'`
 
-# Output of the command looks like:
-#Results for cma_ha_b
-#	:mgStatusOK (0)
-#	:mgSyncStatus (Lagging)
+			if [ $cma_stat = 0 ]
+			then
+			#Fire Check and record the following in the ticket
+			echo "Problematic CMA:" $SECONDARY
+			cpmistat -o schema -r mg $SECONDARY | grep 'mgStatusOK\|mgSync'
+			fi
+			# Output of the command looks like:
+			#Problematic CMA: CMA_Test
+			#:mgStatusOK (0)
+			#:mgSyncStatus (Lagging)
 
+
+
+	done </opt/devmgmt/etc/cp.cma.hapairs
+
+else
+	echo "OK: No HA pairs defined in /opt/devmgmt/etc/cp.cma.hapairs"
+	exit 0
+fi
