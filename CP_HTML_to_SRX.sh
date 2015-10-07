b277e1141eddf1674fa2c6a58fb8fe95f31405f8
craigdods <craig.dods@gmail.com>
2015-10-07 15:37:55 -0400

    Update CP_HTML_to_SRX.sh

--- CP_HTML_to_SRX.sh
+++ CP_HTML_to_SRX.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+#Creates Host Nodes (/32)
 grep "Host Node" $INPUT_FILE -A 1 -B 1 | awk -F[\>] '{print $3}' | sed 's/\<br//g;s/<\/a//g;s/<//g;/^\s*$/d' | awk '{printf "%s"(NR%2?" ":RS),$0}' | awk '{print "set security address-book global address",$1,$2}'
+#Creates networks
+grep "Network" Cambridge.html -A 2 -B 1 | sed '/a\ href/d;/div/d;'| awk -F[\>] '{print $3}' | sed 's/\<br//g;s/<\/a//g;s/<//g;/^\s*$/d;s/\/td//g;/^$/d'
