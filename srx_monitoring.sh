099ce0e1bc58326831e3bace1f3d9502bf595181
craigdods <craig.dods@gmail.com>
2016-05-26 09:58:00 -0400

    Update srx_monitoring.sh

--- srx_monitoring.sh
+++ srx_monitoring.sh
@@ -15,3 +15,15 @@ LOGFILE="INTRA-app-prod-log-$CURRENT_TIME.txt"
 
 /usr/sbin/cli show security flow session node 0 | grep -v Policy | awk '{print $2,$4}' | awk NF > /var/tmp/$LOGFILE
 /usr/sbin/cli show ...whatever
+
+###
+# CPU monitoring script - checks for Idle + userid 
+# Written (hastily) by Craig
+
+while [ true ]
+do
+IDLE=$(/usr/sbin/cli show chassis routing-engine node 1 | grep Idle | awk '{print $2}')
+USERID=$(/usr/sbin/cli show system processes extensive node 1 | grep useridd | awk '{print $10}')
+echo "CPU Idle percentage:$IDLE%, useridd usage:$USERID"
+sleep 15
+doneshow
